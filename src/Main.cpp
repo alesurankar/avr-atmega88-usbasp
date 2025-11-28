c27fd644ec6d7280d4b3cda8d0fe3ef54d67ef30
ales2026 <ale.urankar@gmail.com>
2025-11-28 20:25:01 +0100

    using instances with Pin library

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,19 +1,26 @@
 #include "Pin.hpp"
 #include <util/delay.h>
 
-using led = Pin<'B', 3>;
-using btn = Pin<'D', 5>;
 
 int main()
 {
-  led::setOutput();
-  btn::setInput();
-  
+  pin::pb3 led;
+  pin::pd5 btn;
+
+  led.setOutput();
+  btn.setInput();
+
   while(1) {
-    if (btn::read())
-      led::high();
-    else
-      led::low();
+    if(btn.read()) {
+      led.high();
     }
-    _delay_ms(100);
+    else {
+      led.low();
+    }
+
+    for (int i = 0; i < 100; i++) {
+      led.toggle();
+      _delay_ms(100);
+    }
+  }
 }
--- src/Pin.hpp
+++ src/Pin.hpp
@@ -1,51 +1,76 @@
 #pragma once
 #include <avr/io.h>
 
-template<char PORT, uint8_t BIT>
-class Pin
+namespace pin
 {
-  public:
-    static inline void setOutput()
-    {
-        ddr() |= (1 << BIT);
-    }
-    static inline void setInput()
-    {
-        ddr() &= ~(1 << BIT);
-    }
-    static inline void high()
-    {
-        port() |= (1 << BIT);
-    }
-    static inline void low()
-    {
-        port() &= ~(1 << BIT);
-    }
-    static inline void toggle()
-    {
-        port() ^= (1 << BIT);
-    }
-    static inline bool read()
-    {
-        return pinreg() & (1 << BIT);
-    }
-private:
-    static constexpr volatile uint8_t& ddr()
-    {
-        if constexpr (PORT == 'B') return DDRB;
-        else if constexpr (PORT == 'C') return DDRC;
-        else return DDRD;
-    }
-    static constexpr volatile uint8_t& port()
-    {
-        if constexpr (PORT == 'B') return PORTB;
-        else if constexpr (PORT == 'C') return PORTC;
-        else return PORTD;
-    }
-    static constexpr volatile uint8_t& pinreg()
-    {
-        if constexpr (PORT == 'B') return PINB;
-        else if constexpr (PORT == 'C') return PINC;
-        else return PIND;
-    }
-};
+  template<char PORT, uint8_t BIT>
+  class Pin
+  {
+    public:
+      inline void setOutput()
+      {
+          ddr() |= (1 << BIT);
+      }
+      inline void setInput()
+      {
+          ddr() &= ~(1 << BIT);
+      }
+      inline void high()
+      {
+          port() |= (1 << BIT);
+      }
+      inline void low()
+      {
+          port() &= ~(1 << BIT);
+      }
+      inline void toggle()
+      {
+          port() ^= (1 << BIT);
+      }
+      inline bool read()
+      {
+          return pinreg() & (1 << BIT);
+      }
+  private:
+      static inline volatile uint8_t& ddr()
+      {
+          if constexpr (PORT == 'B') return DDRB;
+          else if constexpr (PORT == 'C') return DDRC;
+          else return DDRD;
+      }
+      static inline volatile uint8_t& port()
+      {
+          if constexpr (PORT == 'B') return PORTB;
+          else if constexpr (PORT == 'C') return PORTC;
+          else return PORTD;
+      }
+      static inline volatile uint8_t& pinreg()
+      {
+          if constexpr (PORT == 'B') return PINB;
+          else if constexpr (PORT == 'C') return PINC;
+          else return PIND;
+      }
+  };
+
+  using pb1 = Pin<'B', 1>;
+  using pb2 = Pin<'B', 2>;
+  using pb3 = Pin<'B', 3>;
+  using pb4 = Pin<'B', 4>;
+  using pb5 = Pin<'B', 5>;
+  using pb6 = Pin<'B', 6>;
+  using pb7 = Pin<'B', 7>;
+  using pc1 = Pin<'C', 1>;
+  using pc2 = Pin<'C', 2>;
+  using pc3 = Pin<'C', 3>;
+  using pc4 = Pin<'C', 4>;
+  using pc5 = Pin<'C', 5>;
+  using pc6 = Pin<'C', 6>;
+  using pc7 = Pin<'C', 7>;
+  using pd1 = Pin<'D', 1>;
+  using pd2 = Pin<'D', 2>;
+  using pd3 = Pin<'D', 3>;
+  using pd4 = Pin<'D', 4>;
+  using pd5 = Pin<'D', 5>;
+  using pd6 = Pin<'D', 6>;
+  using pd7 = Pin<'D', 7>;
+}
